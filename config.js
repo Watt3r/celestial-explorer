b97051ef20de87f457b23bfb66f1c37742bb801f
Watt Erikson <turtle@turtle-coin.com>
2018-10-22 15:16:04 -0700

    Release!

---
+++ config.js
@@ -0,0 +1,18 @@
+var api = 'https://celc.cncoin.cf/daemon';
+var donationAddress = "";
+var blockTargetInterval = 45;
+var coinUnits = 100;
+var symbol = 'CELC';
+var refreshDelay = 30000;
+// pools stats by MainCoins
+var networkStat = {
+ "celc": [
+	["celc.cncoin.cf", "https://cle.cncoin.cf/api/"]
+]
+};
+
+var networkStat2 = {
+    "celc": [
+	[""]
+ ]
+};
