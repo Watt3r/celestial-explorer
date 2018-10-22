572df1069b944a6dbedaa28084fb28e958cc1575
Watt Erikson <turtle@turtle-coin.com>
2018-10-22 16:22:34 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var api = 'https://celc.cncoin.cf/daemon';
 var donationAddress = "";
 var blockTargetInterval = 45;
-var coinUnits = 100;
+var coinUnits = 10000;
 var symbol = 'CELC';
 var refreshDelay = 30000;
 // pools stats by MainCoins
